c5ae9ccb3486b54429c568e14ead4ac1c28ffc28
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-11-13 21:38:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-//your code here
+
+function findMajorityElement(arr) {
+  let candidate;
+  let count = 0;
+
+ 
+  for (const num of arr) {
+    if (count === 0) {
+      candidate = num;
+    }
+
+    if (num === candidate) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+
+  count = 0;
+  for (const num of arr) {
+    if (num === candidate) {
+      count++;
+    }
+  }
+
+  if (count > arr.length / 2) {
+    return candidate;
+  } else {
+   
+   
+    return -1;  of a majority element in another way.
+  }
+}
+
+
+const inputArray = [2, 1, 2];
+const majorityElement = findMajorityElement(inputArray);
+console.log(majorityElement); 
